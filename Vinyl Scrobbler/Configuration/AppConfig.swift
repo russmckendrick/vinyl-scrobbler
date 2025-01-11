45f174aa629bef63da4f7978435765ef158d919e
Russ McKendrick <github@mckendrick.email>
2025-01-11 20:13:33 +0000

    ✨ (AppConfig, project.pbxproj): update app version to 1.0.5 and set MACOSX_DEPLOYMENT_TARGET to 15.0 for improved compatibility and performance. This change ensures the application is aligned with the latest versioning standards and deployment requirements.

--- Vinyl Scrobbler/Configuration/AppConfig.swift
+++ Vinyl Scrobbler/Configuration/AppConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum AppConfig {
     static let name = "Vinyl Scrobbler"
-    static let version = "1.0.2"
+    static let version = "1.0.5"
     static let year = "2025"
     static let author = "Russ McKendrick"
     static let description = "A simple Last.fm scrobbler for vinyl records."
