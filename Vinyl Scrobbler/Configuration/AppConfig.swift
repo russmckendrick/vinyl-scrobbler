9d31ac818f8aeea2307fe55fb1a2174985702243
Russ McKendrick <github@mckendrick.email>
2025-01-19 12:05:31 +0000

    🚀 (project.pbxproj, AppConfig.swift): update project version and marketing version to 1.0.7 to reflect new release

--- Vinyl Scrobbler/Configuration/AppConfig.swift
+++ Vinyl Scrobbler/Configuration/AppConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum AppConfig {
     static let name = "Vinyl Scrobbler"
-    static let version = "1.0.6"
+    static let version = "1.0.7"
     static let year = "2025"
     static let author = "Russ McKendrick"
     static let description = "A simple Last.fm scrobbler for vinyl records."
